72a6a1e2a8d53fb30ad8a2e8d321d2c3f6b53b6c
NotMyself <bobby.johnson@gmail.com>
2011-12-08 09:12:38 -0800

    set up basic project structure

---
+++ src/Tests/RealityTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace SSDNUG.Tests
+{
+    [TestFixture]
+    public class RealityTests
+    {
+        [Test]
+        public void true_should_always_be_true()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
