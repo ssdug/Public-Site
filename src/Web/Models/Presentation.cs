7b3014edb5e546067441b372c0f248b2341b596f
NotMyself <bobby.johnson@gmail.com>
2012-02-26 15:32:14 -0800

    slightly modifying models

--- src/Web/Models/Presentation.cs
+++ src/Web/Models/Presentation.cs
@@ -8,10 +8,16 @@ namespace Web.Models
     public class Presentation
     {
         public string Id { get; set; }
-        public Presenter Presenter { get; set; }
+        public PresenterReference Presenter { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime PresentationDate { get; set; }
         public bool Booked { get; set; }
+    }
+
+    public class PresenterReference
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
     }
 }
