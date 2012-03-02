101a01d76b41f1716c5eaaa35a56f87d9be6dfd7
NotMyself <bobby.johnson@gmail.com>
2012-03-02 11:21:16 -0800

    creating a default project info and wireing it up

--- src/ProjectInfo.cs
+++ src/ProjectInfo.cs
@@ -1,26 +1,13 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Web")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Web")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCompany("SSDNUG")]
+[assembly: AssemblyProduct("SSDNUG Public Website")]
+[assembly: AssemblyCopyright("Copyright © South Sound .NET Users Group 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7d2250ac-ec28-4140-89ce-ae73c46a3549")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +16,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.1")]
 [assembly: AssemblyFileVersion("0.0.0.1")]
