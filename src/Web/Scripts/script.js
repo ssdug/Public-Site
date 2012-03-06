cc6cc18718a942c39dd977a6ea790185ed598be7
NotMyself <bobby.johnson@gmail.com>
2012-03-06 07:51:30 -0800

    updated dump to current values, switched map to static map, cleaned up script.js file.

--- src/Web/Scripts/script.js
+++ src/Web/Scripts/script.js
@@ -1,44 +1,32 @@
-﻿(function ($) {
-  $(".hero-unit:first").fadeIn(500);
+﻿$(function() {
+    $(".hero-unit:first").fadeIn(500);
 
-  $(".hero-unit .btn").click(function (e) {
+    $(".hero-unit .btn").click(function() {
+        
+        var $visibleItem = $('div.hero-unit:visible');
+        var total = $('div.hero-unit').length;
+        var index = $visibleItem.prevAll().length;
 
-    //prevent browser jumping to top
-    e.preventDefault();
+        //if we click next increment current index, else decrease index (we aren't using a previous button)
+        $(this).attr('id') === 'Next' ? index++ : index--;
 
-    //get current visible item
-    var $visibleItem = $('div.hero-unit:visible');
+        //if we are now past the beginning or end show the last or first item
+        if (index === -1) {
+            index = total - 1;
+        }
+        if (index === total) {
+            index = 0;
+        }
 
-    //get total item count
-    var total = $('div.hero-unit').length;
+        $visibleItem.hide();
 
-    //get index of current visible item
-    var index = $visibleItem.prevAll().length;
+        $('div.hero-unit:eq(' + index + ')').fadeIn(500);
 
-    //if we click next increment current index, else decrease index (we aren't using a previous button)
-    $(this).attr('id') === 'Next' ? index++ : index--;
+        return false;
+    });
+});
 
-    //if we are now past the beginning or end show the last or first item
-    if (index === -1) {
-      index = total - 1;
-    }
-    if (index === total) {
-      index = 0
-    }
-
-    //hide current show item
-    $visibleItem.hide();
-
-    //fade in the relevant item
-    $('div.hero-unit:eq(' + index + ')').fadeIn(500);
-
-  });
-
-
-
-})(jQuery);
-
-$(document).ready(function () {
+$(function() {
   $('.slideshow').cycle({
     fx: 'fade'
   });
