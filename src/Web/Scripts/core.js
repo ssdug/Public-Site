89758f59430974e89aa65aa690977d46bedcd236
Bobby Johnson <bobby.johnson@gmail.com>
2012-05-05 16:10:56 -0700

    added require.js and refactored a bit

---
+++ src/Web/Scripts/core.js
@@ -0,0 +1,19 @@
+﻿require.config({
+        baseUrl: '/Scripts/',
+        paths: {
+            bootstrap: 'bootstrap'// Omitted the .js as requireJs will add it automagically
+            // And so on for other frameworks/utils
+        }
+    });
+
+    define(
+        'core',
+        [
+            'jquery',
+            'bootstrap'
+        ],
+        function ($) {
+            // jQuery and bootstrap have been loaded
+            return $;
+        }
+    );
