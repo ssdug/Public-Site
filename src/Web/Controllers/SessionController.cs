9d32c5cdd15efc26ec17420da46e268cae32c4c0
Bobby Johnson <bobby.johnson@gmail.com>
2012-05-05 20:25:34 -0700

    testing login and fetching info

---
+++ src/Web/Controllers/SessionController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Facebook;
+
+namespace Web.Controllers
+{
+    public class SessionController : RavenController
+    {
+        //
+        // GET: /Session/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create(string facebookToken)
+        {
+            var client = new FacebookClient(facebookToken);
+            dynamic user = client.Get("me");
+
+          return Json(user);
+        }
+
+    }
+}
