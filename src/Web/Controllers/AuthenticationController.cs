8e6319fd9e7905b98f93eb7b87933ffd22e9eb26
Bobby Johnson <bobby.johnson@gmail.com>
2012-05-05 19:16:47 -0700

    adding authentication controller

---
+++ src/Web/Controllers/AuthenticationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class AuthenticationController : RavenController
+    {
+        //
+        // GET: /Authentication/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
