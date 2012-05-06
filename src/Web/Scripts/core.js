d0d4101e6a7206a693c409f7dcb8e3930592e820
Bobby Johnson <bobby.johnson@gmail.com>
2012-05-05 17:17:34 -0700

    adding facebook api

--- src/Web/Scripts/core.js
+++ src/Web/Scripts/core.js
@@ -1,19 +1,29 @@
 ﻿require.config({
-        baseUrl: '/Scripts/',
-        paths: {
-            bootstrap: 'bootstrap'// Omitted the .js as requireJs will add it automagically
-            // And so on for other frameworks/utils
-        }
+    baseUrl: '/Scripts/',
+    paths: {
+        bootstrap: 'bootstrap'// Omitted the .js as requireJs will add it automagically
+        // And so on for other frameworks/utils
+    }
+});
+
+define(
+    'core',
+    [
+        'jquery',
+        'bootstrap'
+    ],
+    function ($) {
+        // jQuery and bootstrap have been loaded
+        return $;
     });
 
-    define(
-        'core',
-        [
-            'jquery',
-            'bootstrap'
-        ],
-        function ($) {
-            // jQuery and bootstrap have been loaded
-            return $;
-        }
-    );
+    require(['core', 'facebook'], function ($, facebook) {
+        $(function () {
+            facebook.setup();
+            facebook.ready(function () {
+                FB.getLoginStatus(function() {
+                    console.log('something happened');
+                });
+            });
+        });
+    });
--- src/Web/Scripts/facebook.js
+++ src/Web/Scripts/facebook.js
@@ -1,6 +1,60 @@
-﻿(function () {
-    var e = document.createElement('script'); e.async = true;
-    e.src = document.location.protocol
-    + '//connect.facebook.net/en_US/all.js';
-    document.getElementById('fb-root').appendChild(e);
-} ());
+﻿define(['core'], function ($) {
+    var onReadyQueue = [];
+
+    var facebook = {};
+
+    facebook.options = {
+        appId: "188145164542799",
+        cookie: true,
+        status: true,
+        xfbml: true,
+        oauth: true,
+        reloadIfSessionStateChanged: true
+    };
+
+    facebook.ready = function (callback) {
+        if (typeof callback != 'function') {
+            throw new Error('callback must be a function');
+        }
+        onReadyQueue.push(callback);
+    };
+
+    facebook.setup = function (cfg) {
+        $.extend(facebook.options, cfg);
+
+        facebook.ready(function () {
+            FB.init(facebook.options);
+        });
+
+        window.fbAsyncInit = function () {
+            for (var i = 0, l = onReadyQueue.length; i < l; i += 1) {
+                onReadyQueue[i]();
+            }
+
+            facebook.ready = function (callback) {
+                if (typeof callback != 'function') {
+                    throw new Error('callback must be a function');
+                }
+                callback();
+            };
+        };
+
+        if (typeof FB === 'object') {
+            window.fbAsyncInit();
+        } else {
+            var e = document.createElement('script'); e.async = true;
+            e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+            document.getElementById('fb-root').appendChild(e);
+        }
+
+        return this;
+    };
+
+    facebook.reparse = function (scope) {
+        facebook.ready(function () {
+            FB.XFBML.parse(scope);
+        });
+    };
+
+    return facebook;
+});
