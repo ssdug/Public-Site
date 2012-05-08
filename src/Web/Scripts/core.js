5d75d87fa0f3b298fb723a208d95286098cab0a4
Bobby Johnson <bobby.johnson@gmail.com>
2012-05-08 14:04:26 -0700

    some experiemental facebook code

--- src/Web/Scripts/core.js
+++ src/Web/Scripts/core.js
@@ -21,9 +21,19 @@ define(
         $(function () {
             facebook.setup();
             facebook.ready(function () {
-                FB.getLoginStatus(function() {
-                    console.log('something happened');
+                FB.getLoginStatus( function (response) {
+                    if (response.status === 'connected') {
+                        var uid = response.authResponse.userID;
+                        var accessToken = response.authResponse.accessToken;
+                        console.log("Connected as " + uid + " with access token " + accessToken);
+                    } else if (response.status === 'not_authorized') {
+                        console.log("The user is logged into Facebook but app is not authorized");
+                    } else {
+                        console.log("The user is not logged into Facebook.");
+                    }
+
                 });
+
             });
         });
     });
